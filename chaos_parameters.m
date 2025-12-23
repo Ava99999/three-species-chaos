821e05160cb81a8c33009b596dd4642ad69727fa
Ava99999 <149510030+Ava99999@users.noreply.github.com>
2025-12-23 22:45:15 +0100

    finished matlab code for 01 test

--- chaos_parameters.m
+++ chaos_parameters.m
@@ -1,10 +1,10 @@
 % system
 param.a1 = 5;
 param.a2 = 0.1;
-param.b1 = 3; % varies between 2 and 6.2
+param.b1 = 2.25; %3; % varies between 2 and 6.2
 param.b1_r = 3.6:0.001:4; %2.2:0.01:6.2; %2.25:0.001:2.6;  %2.9:0.01:4.2; 
-param.b2 = 2;
-param.b2_r = 1.5:0.001:3.2;
+param.b2 = 1.6; %2;
+param.b2_r = 1.5:0.01:3.2;
 param.d1 = 0.4; 
 param.d2 = 0.01;
 %param.ic1 = [0.76, 0.16, 9.9]; % specific to reproduction figure 2
@@ -14,13 +14,14 @@ param.ic1 = [1, 1, 1];
 % p = [5.0, 0.1, 3.0, 2.0, 0.4, 0.01] # values for a1, a2, b1, b2, d1 and d2 
 
 % time steps
-param.N = 10000; 
-param.T = 10000;
+param.N = 100000; %10000; 
+param.T = 100000; %10000;
 param.dt = param.T/param.N;
 param.t = [0:(param.N -1)]*param.dt;
 
 % bifurcation plot
 param.NB = 1000; %N_Bifurcation
+param.NT = param.N/10; %N_01 test 
 
 % 01 test
 %param.data = 500000;
@@ -29,5 +30,5 @@ param.NB = 1000; %N_Bifurcation
 %param.tsteps = [0:(param.tau -1)]*param.delt;
 param.ic2 = [1,1,1];
 param.sizec = 100;
-c = pi/5 + 3*pi/5.*rand(1,param.sizec);
-param.c = sort(c);
+help_c = pi/5 + 3*pi/5.*rand(1,param.sizec);
+param.c = sort(help_c);
